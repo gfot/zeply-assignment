477445abd8913e5f9b949751a6e5b51d79623720
gfot <giorgosfm@yahoo.gr>
2023-01-24 11:53:00 +0200

    Initial commit (boilerplate)

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { resolve as pathResolve } from 'path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+
+const resolve = (path: string) => pathResolve(__dirname, path)
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '~': resolve('frontend'),
+    },
+  },
+  test: {
+    globals: true,
+    watch: false,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    resolveSnapshotPath: (testPath: any, snapExtension: any) =>
+      testPath + snapExtension,
+  },
+})
