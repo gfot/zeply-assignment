d66d25d33132dfe0cb5310d3a35b02a37e6dfc34
gfot <giorgosfm@yahoo.gr>
2023-01-24 11:54:02 +0200

    Add backend vite configuration override

---
+++ backend/vite.config.js
@@ -0,0 +1,14 @@
+import { mergeConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+
+import viteConfig from '../vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      exclude: ['test/support/*'],
+      environment: 'node',
+    },
+  })
+)
